260413867fd1e14746da9f9118f4713e921e4009
Zhenxu Wang <mason123wp@gmail.com>
2023-05-13 21:57:09 -0400

    updated main()

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	nutriScore := GetNuTritionalScore(NutritionalData{
+		Energy:              EnergyFromKcal(),
+		Sugar:               SugarGram(0),
+		SaturatedFattyAcids: SaturatedFattyAcids(),
+		Sodium:              SodiumMilligram(),
+		Fruits:              FruitsPercent(),
+		Fibre:               FibreGram(),
+		Protein:             ProteinGram(),
+	}, Food)
+
+	fmt.Printf("Nutritional Score: %d\n", nutriScore.Value)
+}
